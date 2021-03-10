b831e32bc83d9fae9598f846b0dd522485cd514b
ezequiel hernandez <32853621+drandroidsv@users.noreply.github.com>
2021-03-09 23:00:18 -0600

    Add files via upload

---
+++ requerimentos.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#mensaje y logo bienvenida
+clear
+toilet --filter border Instalador | lolcat
+echo
+echo -e "$purple(*)$blue encriptador $Cafe v2.0$blue"
+sleep 2
+echo -e "$purple(*)$blue Power By$purple mastersv"
+sleep 1
+echo -e "$purple(*)$blue Sigueme en twitter @MASTERSV19$Cafe"
+
+export PS3=$'\e[01;35m(*)\e[01;32m Elige una Opcion:\e[01;33m '
+
+sleep 2
+
+echo -e "$purple(*)$blue Adquiriendo$purple permisos"
+
+sleep 5
+sudo apt install git
+sudo apt install python
+sudo apt install nodejs
+sudo apt install npn
+npm install -g bash-obfuscate
+git clone https://github.com/servitechsv/encriptador.git
+cd encriptador
+python2 encriptador.sh
+
